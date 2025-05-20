3882bdaff1343ca51585f52c5632f408c9d798f8
Furqan Uppal <92501915+furqanuppal@users.noreply.github.com>
2025-05-20 15:31:00 +0500

    Update init_database.sql

--- scipts/init_database.sql
+++ scipts/init_database.sql
@@ -19,9 +19,122 @@ GO
 -- Create Schemas
 CREATE SCHEMA bronze;
 GO
-
 CREATE SCHEMA silver;
 GO
-
 CREATE SCHEMA gold;
 GO
+
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE
+);
+GO
+
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE
+);
+GO
+
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id NVARCHAR(50),
+	sls_order_dt NVARCHAR(50),
+	sls_ship_dt NVARCHAR(50),
+	sls_due_dt NVARCHAR(50),
+	sls_sales NVARCHAR(50),
+	sls_quantity NVARCHAR(50),
+	sls_price NVARCHAR(50)
+);
+GO
+
+CREATE TABLE bronze.erp_CUST_AZ12 (
+	CID NVARCHAR(50),
+	BDATE DATE,
+	GEN NVARCHAR(50)
+);
+GO
+
+CREATE TABLE bronze.erp_LOC_A101 (
+	CID NVARCHAR(50),
+	CNTRY NVARCHAR(50)
+);
+GO
+
+CREATE TABLE bronze.erp_PX_CAT_G1V2 (
+	ID NVARCHAR(50),
+	CAT NVARCHAR(50),
+	SUBCAT NVARCHAR(50),
+	MAINTENANCE NVARCHAR(50)
+);
+GO;
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	TRUNCATE TABLE bronze.crm_cust_info
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\temp\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE bronze.crm_prd_info
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\temp\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE bronze.crm_sales_details
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\temp\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE bronze.erp_CUST_AZ12
+	BULK INSERT bronze.erp_CUST_AZ12
+	FROM 'C:\temp\CUST_AZ12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE bronze.erp_LOC_A101
+	BULK INSERT bronze.erp_LOC_A101
+	FROM 'C:\temp\LOC_A101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	TRUNCATE TABLE bronze.erp_PX_CAT_G1V2
+	BULK INSERT bronze.erp_PX_CAT_G1V2
+	FROM 'C:\temp\PX_CAT_G1V2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+END
+
+EXEC bronze.load_bronze
